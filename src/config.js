2b9523d7dcaf5fc1e9cebbda9188133283217530
ItsOnlyBinary <git@itsonlybinary.com>
2024-11-08 15:33:05 +0000

    update config.js

--- src/config.js
+++ src/config.js
@@ -1,15 +1,16 @@
 /* global kiwi:true */
 
-const configBase = 'plugin-template';
-const defaultConfig = {
+export const basePath = getBasePath();
+export const configBase = 'plugin-template';
+export const defaultConfig = {
 };
 
 export function setDefaults(kiwi) {
     kiwi.setConfigDefaults(configBase, defaultConfig);
 }
 
-export function setting(name) {
-    return kiwi.state.setting([configBase, name].join('.'));
+export function setting(name, newVal) {
+    return kiwi.state.setting([configBase, name].join('.'), newVal);
 }
 
 export function getSetting(name) {
@@ -18,4 +19,10 @@ export function getSetting(name) {
 
 export function setSetting(name, value) {
     return kiwi.state.setSetting(['settings', configBase, name].join('.'), value);
+}
+
+function getBasePath() {
+    const scripts = document.getElementsByTagName('script');
+    const scriptPath = scripts[scripts.length - 1].src;
+    return scriptPath.substr(0, scriptPath.lastIndexOf('/') + 1);
 }
